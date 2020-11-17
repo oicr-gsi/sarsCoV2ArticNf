fd7567878c24d91328fe00005d89a876b0c1db2c
Rishi Shah <rshah@oicr.on.ca>
2020-11-17 13:39:18 -0500

    update

---
+++ calculate.sh
@@ -0,0 +1,1 @@
+find . \( -type f -size +0 -iname "*.pdf" \) -printf "pdf file exists\n";
---
+++ compare.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+#actual output metrics is $1, expected output metrics is $2
+diff $1 $2
